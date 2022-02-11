c1315179cce012555a1f3806aa172cc471ac5036
Paidamoyo Janet MYLES Azehko <78198093+ProjectErostribe@users.noreply.github.com>
2022-02-10 20:07:37 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Linking } from 'react-native';
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Main from './Components/Main';
+import About from './Components/About';
+
 
 export default function App() {
+
+  const initialState = {
+    winningStreak: 0
+  }
+
+  const reducer = (state = initialState, action) => {
+    switch(action.type){
+      case 'INCREASE_WINNING_STREAK':
+        return {
+          winningStreak: state.winningStreak + 1
+        }
+      case 'RESET_WINNING_STREAK':
+        return {
+          winningStreak: 0
+        }
+    }
+    return state;
+  }
+
+  const store = createStore(reducer);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+      <Header />
+      <Main />
+      <Footer />
       <StatusBar style="auto" />
-    </View>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
